c2f4bad06e5405a5f6f83235c49f17d51f859b07
Kyle Joseph Timajo <timajokyle24@gmail.com>
2022-05-25 18:54:18 +0800

    fix: img alt tags error

--- personal-pages/kyle/kyle.js
+++ personal-pages/kyle/kyle.js
@@ -21,7 +21,7 @@ function KylePage() {
             <div className="profile-image text-center mb-3">
               <img
                 src={ProfileImage}
-                alt="timajo profile image"
+                alt="timajo-profile"
                 className="timajo-image position-relative"
               />
             </div>
