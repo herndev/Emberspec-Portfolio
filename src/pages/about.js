fd5a9212f49997da47a4bcdd7bb8bb847d51722b
herndev <herniejabien45@gmail.com>
2022-05-26 15:06:00 +0800

    Updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 
 function About() {
 
