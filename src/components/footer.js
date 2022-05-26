a141b19b943cdd6f4187df66cf33ec4bf1140acf
herndev <herniejabien45@gmail.com>
2022-05-26 13:59:38 +0800

    Updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div id="footer" className="mt-4 p-3 bg-dark crv-8 text-white bx-shadow footer display-centery rm-md-0 rcrv-md-0">
-            <h5 className="text-warning font-weight-bold hidden-lg">
+        <div id="footer" className="mt-4 p-3 bg-dark crv-8 text-white bx-shadow footer display-center rm-md-0 rcrv-md-0">
+            {/* <h5 className="text-warning font-weight-bold hidden-lg">
                 Let's make your ideas possible !!
-                        </h5>
+                        </h5> */}
             <small className="copyright-text">
-                Copyright © 2020 Hernie Jabien
+                Copyright © 2022 Emberspec
                     </small>
         </div>
     );
